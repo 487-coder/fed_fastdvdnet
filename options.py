c51dc708a19b00c82c0f861a51c04c5525f6112b
xia <3415671317@qq.com>
2025-08-02 11:10:56 +1000

    Fix test_psnr unpack bug and unify testloader handling

--- options.py
+++ options.py
@@ -7,21 +7,21 @@ def args_parser():
     # Data specifc paremeters
     parser.add_argument('--train_dataset', default='./mp4',
                         help='train file root')
-    parser.add_argument('--client_numbers', default=10,
+    parser.add_argument('--client_numbers', type= int, default=10,
                         help='client number')
     parser.add_argument('--test_dataset', default='./480p',
                         help='test file root')
-    parser.add_argument('--patch_size', default='96',
+    parser.add_argument('--patch_size',type= int default=96,
                         help='Patch size')
     parser.add_argument("--batch_size", type=int, default=64,
                         help="Training batch size")
-    parser.add_argument("--client_noise_level", default=[5,55],
+    parser.add_argument("--client_noise_level",nargs=2, type=int,default=[5,55],
                         help="Noise training interval")
     parser.add_argument("--client_noise_type", default="Gaussian",
                         help="noise type")
     parser.add_argument('--upload_model', action="store_true", default=False,
                         help='allow clients to upload models to the server')
-    parser.add_argument('--test_noise', default=25,
+    parser.add_argument('--test_noise',type = float, default=25,
                         help='noise level used on validation set')
     parser.add_argument("--temp_psz", "--tp", type=int, default=5,
                         help="Temporal patch size")
